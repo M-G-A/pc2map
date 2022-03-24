fe8d014a6819ce3e226c3f18232ba5e1675d9ab4
M-G-A <michi.adam@gmx.de>
2022-03-24 15:32:05 +0100

    Moves the alignment of the data to a separate function. If normals exist, they are used for orientation estimation.

--- pc2map.py
+++ pc2map.py
@@ -3,6 +3,28 @@ import numpy as np
 import open3d as o3d
 import cv2
 
+def alignData(pc,R=None):
+    if R is None:
+        if pc.has_normals():
+            N = 90
+            xy = np.asarray(pc.normals)[:,0:2].T
+            ang = np.arctan2(xy[0],xy[1])%(np.pi/2) # heading angles of the normals
+            alpha,b = np.histogram(ang,N,range=(0,np.pi/2)) 
+            alpha = (alpha.argmax()+0.5)/N*np.pi/2 # mode of histogram
+            c=np.cos(alpha)
+            s=np.sin(alpha)
+            R=np.array([[c,-s,0],
+                        [s,c,0],
+                        [0,0,1]])
+        else:
+            from sklearn.decomposition import PCA
+            xy = np.asarray(pc.points)[:,0:2]
+            pca = PCA(n_components=2)
+            pca.fit_transform(xy)
+            R = np.eye(3);
+            R[0:2,0:2] = pca.components_
+    pc.rotate(R)
+
 def pc2map(pc, map_out=None, res=5, height=1.8):
     p = np.array(pc.points)
 
@@ -14,13 +36,6 @@ def pc2map(pc, map_out=None, res=5, height=1.8):
     n,bins = np.histogram(z,50)
     floor = bins[np.argmax(n)+1]
     index = (z>floor) & (z<floor+height)
-
-    # remove Rotation
-    ## TODO: use histogram of normals (if existing) 
-    from sklearn.decomposition import PCA
-    pca = PCA(n_components=2)
-    pca.fit(XY[~index,:])
-    XY = pca.transform(XY)
 
     # quantize and scale xy of points
     xy = XY*100.0/res
@@ -48,8 +63,10 @@ if __name__ == "__main__":
 
     # read pc
     pc = o3d.io.read_point_cloud(pc_path)
+    
+    # uncomment if you don't want to align the data
+    # you can also set R to a desired value
+    alignData(pc,R=None)
 
     # create map
     pc2map(pc,map_out)
-
-
